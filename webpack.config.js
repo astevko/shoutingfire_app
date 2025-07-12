7a84cdf45e33abcb6308cb6ac7ba98540a8d3acb
Andrew Stevko <andy.stevko@gmail.com>
2025-07-12 11:48:50 -0700

    webpack working again

--- webpack.config.js
+++ webpack.config.js
@@ -3,23 +3,14 @@ const createExpoWebpackConfigAsync = require('@expo/webpack-config');
 module.exports = async function (env, argv) {
   const config = await createExpoWebpackConfigAsync(env, argv);
   
-  // Add polyfills
+  // Add polyfills for Node.js modules
   config.resolve.fallback = {
     ...config.resolve.fallback,
     crypto: require.resolve('crypto-browserify'),
     stream: require.resolve('stream-browserify'),
     buffer: require.resolve('buffer'),
-    process: require.resolve('process/browser'),
+    vm: require.resolve('vm-browserify'),
   };
-
-  // Add plugins
-  const webpack = require('webpack');
-  config.plugins.push(
-    new webpack.ProvidePlugin({
-      Buffer: ['buffer', 'Buffer'],
-      process: 'process/browser',
-    })
-  );
 
   return config;
 }; 
